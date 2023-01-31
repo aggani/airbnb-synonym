2524b6c0300b9fe2ba258eb2c947b7884262d160
Anirudh <iamanirudhaggarwal@gmail.com>
2023-02-01 00:14:48 +0530

    initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,21 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+
+const Home: NextPage = () => {
+  return ( 
+    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+      <Head>
+        <title>airbnb</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <h1>Lets build the airbnb-synonym</h1>
+      
+      </Header>
+      
+    </div>
+  )
+}
+
+export default Home
