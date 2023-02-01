c9bcbac4793c28ac9b46c12a15be79dfd71f615b
Anirudh <iamanirudhaggarwal@gmail.com>
2023-02-01 17:54:51 +0530

    left header created

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import Image from "next/image";
 
 function Header() {
   return (
-    <div>Header</div>
+    <header className="sticky top-0 z-50 p-5 grid-cols-3 shadow-lg md:px-10">
+
+        {/* Left */}
+        <div className="relative flex items-center h-10 cursor-pointer my-auto " >
+          <Image 
+            src='https://links.papareact.com/qd3' 
+            layout="fill"
+            objectFit="contain"
+            objectPosition="left"
+          />
+        </div>
+
+        {/* Middle - search section*/}
+        <div>
+          
+        </div>
+
+        {/* Right */}
+        <div>
+
+        </div>
+    </header>
   )
 }
 
-export default Header
+export default Header;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,17 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import Header from '../components/Header'
 
 const Home: NextPage = () => {
   return ( 
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div >
       <Head>
         <title>airbnb</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <h1>Lets build the airbnb-synonym</h1>
       
-      </Header>
+      <Header />
       
     </div>
   )
