389bcfa7e5012265ff04524c3c98d1d5f33abeb0
Anirudh <iamanirudhaggarwal@gmail.com>
2023-02-01 22:55:40 +0530

    search box created

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-
+import {MagnifyingGlassIcon} from "@heroicons/react/24/solid"
 function Header() {
   return (
     <header className="sticky top-0 z-50 p-5 grid-cols-3 shadow-lg md:px-10">
@@ -15,13 +15,14 @@ function Header() {
         </div>
 
         {/* Middle - search section*/}
-        <div>
-          
+        <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-lg">
+          <input className="pl-5 outline-none flex-grow text-sm text-gray-600" type="text" placeholder="Start your search for breath-taking stays"/>
+          <MagnifyingGlassIcon className="hidden md:inline-flex h-10 bg-red-400 text-white rounded-full p-1.5 cursor-pointer md:mx-2"/>
         </div>
 
         {/* Right */}
         <div>
-
+          <p>Become a host</p>
         </div>
     </header>
   )
