ec623a61e3438e4c0f4868fce17ba629a990da57
Anirudh <iamanirudhaggarwal@gmail.com>
2023-03-01 17:44:49 +0530

    SmallCards updated

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,14 +21,17 @@ export default function Home({ exploreData }) {
           <h2 className='text-4xl font-semibold'>Explore Nearby</h2>
           
           {/* Pull data from API end points */ }
-          {exploreData.map(({ img, distance, location }) =>(
-            <SmallCard 
-              key={img}
-              img = {img}
-              distance = {distance}
-              location = {location}
-            />
-          ))}
+          <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
+            {exploreData.map(({ img, distance, location }) =>(
+              <SmallCard 
+                key={img}
+                img = {img}
+                distance = {distance}
+                location = {location}
+              />
+            ))}
+          </div>
+          
            
         </section>
       </main>
