c8a21303fb6847b836c2232f7059040164369452
Anirudh <iamanirudhaggarwal@gmail.com>
2023-03-01 17:04:06 +0530

    exploreData added

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 import Banner from '../components/Banner'
 import Header from '../components/Header'
 
-const Home: NextPage = () => {
+export default function Home({ exploreData }) {
   return ( 
     <div >
       <Head>
@@ -15,12 +15,31 @@ const Home: NextPage = () => {
       <Header />
       <Banner />
 
-      <main>
-        
+      <main className='max-w-7xl mx-auto px-8 sm:px-16'>
+        <section className='pt-6'>
+          <h2 className='text-4xl font-semibold'>Explore Nearby</h2>
+          {/* Pull data from API end points */ }
+          {exploreData.map((item) => (
+            <h1>{item.location}</h1>
+          ))}
+           
+        </section>
       </main>
       
     </div>
   )
 }
 
-export default Home
+export async function getStaticProps() {
+  const exploreData = await fetch('https://www.jsonkeeper.com/b/IPBM').
+  then(
+    (res) => res.json()
+  );
+
+  return{
+      // props ek object hai, jisme key-value pairs hain 
+    props: {
+      exploreData,  // exploreData: exploreData
+    },
+  };
+}
